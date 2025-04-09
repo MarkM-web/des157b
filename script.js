10a36996dbe5c0bb352e98dea58140e1d3b5245d
Mark Ma PC <tymma@ucdavis.edu>
2025-04-09 14:52:28 -0700

    toggle done

---
+++ script.js
@@ -0,0 +1,37 @@
+(function(){
+    'use strict';
+    console.log('reading js');
+
+    const listOfTogglable = document.querySelectorAll('.toggable');
+    const myButton = document.querySelector('.toggle');
+    const myHeader = document.querySelector('header img');
+    let darkMode = true;
+    // console.log(listOfTogglable);
+     
+    myButton.addEventListener('click', function(){
+        console.log('button clivked');
+        toggleColors()
+    });
+
+    function toggleColors(){
+        if (darkMode){
+            //switch to light
+            for (let i = 0; i < listOfTogglable.length; i++){
+                listOfTogglable[i].classList = 'toggable light'
+            }
+            myButton.classList = 'lightbtn';
+            myHeader.src = "images/header_light.svg";
+            darkMode = false;
+        } else {
+            //switch to dark
+            for (let i = 0; i < listOfTogglable.length; i++){
+                listOfTogglable[i].classList = 'toggable dark'
+            }
+            myButton.classList = 'darkbtn';
+            myHeader.src = "images/header_dark.svg";
+
+            darkMode = true;
+
+        }
+    }
+})();
